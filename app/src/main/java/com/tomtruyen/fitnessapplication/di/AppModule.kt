e8635583df4516b1a99924cb7d434523ee3ad65c
Tom Truyen <tom.truyen@gmail.com>
2023-08-09 21:18:48 +0200

    feat: add UserRepository to DI

--- app/src/main/java/com/tomtruyen/fitnessapplication/di/AppModule.kt
+++ app/src/main/java/com/tomtruyen/fitnessapplication/di/AppModule.kt
@@ -1,7 +1,10 @@
 package com.tomtruyen.fitnessapplication.di
 
+import com.tomtruyen.fitnessapplication.repositories.UserRepositoryImpl
+import com.tomtruyen.fitnessapplication.repositories.interfaces.UserRepository
 import org.koin.dsl.module
 
 val appModule = module {
-
+    // Repositories
+    single<UserRepository> { UserRepositoryImpl() }
 }
