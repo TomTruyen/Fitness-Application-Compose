2301ca418113249c6690f1d87d9be7d0f4caba45
Tom Truyen <tom.truyen@gmail.com>
2023-08-16 16:24:04 +0200

    style: bottom navigation styling

--- app/src/main/java/com/tomtruyen/fitnessapplication/navigation/MainBottomNavigation.kt
+++ app/src/main/java/com/tomtruyen/fitnessapplication/navigation/MainBottomNavigation.kt
@@ -3,12 +3,15 @@ package com.tomtruyen.fitnessapplication.navigation
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FitnessCenter
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -21,7 +24,9 @@ fun MainBottomNavigation(
     val backstackEntry = navController.currentBackStackEntryAsState()
 
     if(showBottomBar) {
-        NavigationBar {
+        NavigationBar(
+            containerColor = Color.Transparent,
+        ) {
             BottomNavigation.items.forEach { item ->
                 val selected = item.route == backstackEntry.value?.destination?.route
 
@@ -43,7 +48,11 @@ fun MainBottomNavigation(
                                 Modifier
                             }
                         )
-                    }
+                    },
+                    colors = NavigationBarItemDefaults.colors(
+                        indicatorColor = MaterialTheme.colorScheme.tertiary,
+                        selectedIconColor = MaterialTheme.colorScheme.onTertiary
+                    )
                 )
             }
         }
