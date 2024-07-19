605724fccf1b8aba55ec525b350c2f63484a80c0
Tom Truyen <tom.truyen@gmail.com>
2024-07-19 20:37:41 +0200

    style: update theme

--- core/designsystem/src/main/java/com/tomtruyen/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/tomtruyen/core/designsystem/theme/Color.kt
@@ -9,7 +9,6 @@ val DarkCharcoal = Color(0xFF2F2F2F)
 val BlueGrey = Color(0xFF64748B)
 val LavenderMist = Color(0xFFE6E6FA)
 val Cultured = Color(0xFFF6F6F6)
-val Topaz = Color(0xFFFFCB74)
 
 // Error Colors
 val LighterRed = Color(0xFFFF5555)
