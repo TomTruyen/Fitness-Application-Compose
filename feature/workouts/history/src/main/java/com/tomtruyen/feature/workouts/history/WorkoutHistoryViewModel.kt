3e891fd575031a197850eaf9fd42513d881c965a
Tom Truyen <tom.truyen@gmail.com>
2025-02-22 22:27:43 +0100

    docs: add a todo

--- feature/workouts/history/src/main/java/com/tomtruyen/feature/workouts/history/WorkoutHistoryViewModel.kt
+++ feature/workouts/history/src/main/java/com/tomtruyen/feature/workouts/history/WorkoutHistoryViewModel.kt
@@ -11,6 +11,7 @@ class WorkoutHistoryViewModel(
 ): BaseViewModel<WorkoutHistoryUiState, WorkoutHistoryUiAction, WorkoutHistoryUiEvent>(
     initialState = WorkoutHistoryUiState()
 ) {
+    // TODO: Add SwipeToRefresh logic - Also see if we can improve this to use our `BaseRepository.fetch()` caching logic instead of having to fetch firebase data directly each time
     val history = workoutHistoryRepository.getWorkoutHistoriesPaginated(
         userRepository.getUser()?.uid.orEmpty()
     ).cachedIn(vmScope)
