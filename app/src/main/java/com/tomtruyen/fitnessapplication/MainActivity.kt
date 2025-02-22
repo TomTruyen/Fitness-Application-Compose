c39d42cb36dc26310d861613e00797050138df0e
Tom Truyen <tom.truyen@gmail.com>
2025-02-22 13:58:52 +0100

    feat: enable edgetoedge

--- app/src/main/java/com/tomtruyen/fitnessapplication/MainActivity.kt
+++ app/src/main/java/com/tomtruyen/fitnessapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -47,6 +48,9 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
         setContent {
             FitnessApplicationTheme {
                 val navController = rememberNavController()
--- core/designsystem/src/main/java/com/tomtruyen/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/tomtruyen/core/designsystem/theme/Theme.kt
@@ -1,15 +1,10 @@
 package com.tomtruyen.core.designsystem.theme
 
-import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = lightColorScheme(
     primary = ChineseBlack,
@@ -55,14 +50,6 @@ fun FitnessApplicationTheme(
     val colorScheme = when {
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        val currentWindow = (view.context as Activity).window
-
-        SideEffect {
-            WindowCompat.getInsetsController(currentWindow, view).isAppearanceLightStatusBars = !darkTheme
-        }
     }
 
     MaterialTheme(
