7301877c0e149fd39a749406b69a759247c5b8dc
Tom Truyen <tom.truyen@gmail.com>
2025-03-02 19:45:54 +0100

    fix: format of names

--- core/common/src/main/java/com/tomtruyen/core/common/utils/ImageLoader.kt
+++ core/common/src/main/java/com/tomtruyen/core/common/utils/ImageLoader.kt
@@ -1,5 +1,6 @@
 package com.tomtruyen.core.common.utils
 
+import android.content.Context
 import io.github.jan.supabase.storage.Storage
 
 class ImageLoader(
@@ -9,5 +10,15 @@ class ImageLoader(
         private const val BUCKET_ID = "Exercises"
     }
 
-    fun load(url: String?) = url?.let { storage.from(BUCKET_ID).publicUrl(it) }
+    fun load(context: Context, url: String?) = url?.let {
+        fromLocal(context, url) ?: storage.from(BUCKET_ID).publicUrl(it)
+    }
+
+    private fun fromLocal(context: Context, url: String): String? {
+        val resourceId = context.resources.getIdentifier(url, "drawable", context.packageName)
+
+        return if(resourceId != 0) {
+            "android.resource://${context.packageName}/$resourceId"
+        } else null
+    }
 }
