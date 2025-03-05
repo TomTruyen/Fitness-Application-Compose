6401f7b6cc8665f0a122b616c4be06c157987aec
Tom Truyen <tom.truyen@gmail.com>
2025-03-05 20:45:40 +0100

    fix: exercise header click interaction

--- feature/workouts/manage/src/main/java/com/tomtruyen/feature/workouts/manage/components/WorkoutExerciseHeader.kt
+++ feature/workouts/manage/src/main/java/com/tomtruyen/feature/workouts/manage/components/WorkoutExerciseHeader.kt
@@ -1,6 +1,6 @@
 package com.tomtruyen.feature.workouts.manage.components
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,8 +11,14 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -28,6 +34,8 @@ fun WorkoutExerciseHeader(
     onActionClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    var isPressed by remember { mutableStateOf(false) }
+
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start),
@@ -45,9 +53,18 @@ fun WorkoutExerciseHeader(
             style = MaterialTheme.typography.bodyLarge.copy(
                 fontWeight = FontWeight.Bold
             ),
-            modifier = Modifier.clickable(
-                onClick = onTitleClick
-            )
+            modifier = Modifier
+                .alpha(if(isPressed) 0.5f else 1f)
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onPress = {
+                            isPressed = true
+                            tryAwaitRelease()
+                            onTitleClick()
+                            isPressed = false
+                        }
+                    )
+                }
         )
 
         Spacer(modifier = Modifier.weight(1f))
