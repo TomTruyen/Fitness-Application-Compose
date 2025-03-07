ff7f7f3506c60d7c0648cd765f1cd78ab25a7892
Tom Truyen <tom.truyen@gmail.com>
2025-03-07 22:27:36 +0100

    feat: add sql query filtering to reduce traffic

--- supabase/rpc/get_user_latest_sets_for_exercises.sql
+++ supabase/rpc/get_user_latest_sets_for_exercises.sql
@@ -2,19 +2,17 @@
 --- It works by using a list of 'exercise_ids' as input and uses the current authenticated user to ensure only their data is fetched
 --- It then JOINS the required tables and creates a partition using the 'exercise_id' and 'sort_order'
 --- This will then return all the latest sets for a specific exercise, for all setIndices (sort_order) that exist
-
-CREATE OR REPLACE FUNCTION get_user_latest_sets_for_exercises(
-    exercise_ids uuid[]  -- Input: List of exercise_ids
-)
-RETURNS TABLE(
-    id uuid,
-    exercise_id uuid,
-    reps bigint,
-    weight double precision,
-    "time" bigint,
-    sort_order bigint,
-    created_at timestamp with time zone
-) AS $$
+create or replace function get_user_latest_sets_for_exercises (
+  exercise_ids uuid[] -- Input: List of exercise_ids
+) RETURNS table (
+  id uuid,
+  exercise_id uuid,
+  reps bigint,
+  weight double precision,
+  "time" bigint,
+  sort_order bigint,
+  created_at timestamp with time zone
+) as $$
 WITH RankedSets AS (
     SELECT
         whes.id,
@@ -24,6 +22,7 @@ WITH RankedSets AS (
         whes.time,
         whes.sort_order,
         whes.created_at,
+        whe.type AS exercise_type,
         ROW_NUMBER() OVER (PARTITION BY whe.exercise_id, whes.sort_order ORDER BY whes.created_at DESC) AS rn -- Partition to get a combined "hash key" using exercise_id and sort_order
     FROM "public"."WorkoutHistoryExerciseSet" whes
     JOIN "public"."WorkoutHistoryExercise" whe ON whe.id = whes.workout_history_exercise_id
@@ -33,5 +32,12 @@ WITH RankedSets AS (
 )
 SELECT id, exercise_id, reps, weight, time, sort_order, created_at
 FROM RankedSets
-WHERE rn = 1;
+WHERE rn = 1
+-- Filtering based on exercise type:
+AND (
+    -- For exercises of type "Time", ensure time is not null
+    (exercise_type = 'Time' AND time IS NOT NULL)
+    -- For exercises of type "Weight", ensure both weight and reps are not null
+    OR (exercise_type = 'Weight' AND weight IS NOT NULL AND reps IS NOT NULL)
+);
 $$ LANGUAGE sql STABLE;
