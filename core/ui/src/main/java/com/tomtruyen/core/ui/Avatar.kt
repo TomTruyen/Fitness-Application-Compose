4dd44eea984e66728aee257df73bd5b3ae365a8d
Tom Truyen <tom.truyen@gmail.com>
2025-03-09 21:13:18 +0100

    refactor: cleanup

--- core/ui/src/main/java/com/tomtruyen/core/ui/Avatar.kt
+++ core/ui/src/main/java/com/tomtruyen/core/ui/Avatar.kt
@@ -2,7 +2,6 @@ package com.tomtruyen.core.ui
 
 import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
@@ -10,10 +9,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -27,9 +22,7 @@ import coil.compose.AsyncImage
 import com.tomtruyen.core.common.R
 import com.tomtruyen.core.common.utils.ImageLoader
 import com.tomtruyen.core.designsystem.Dimens
-import com.tomtruyen.core.designsystem.theme.datastore.ThemePreferencesDatastore
 import com.tomtruyen.core.designsystem.theme.fallbackImageBackground
-import com.tomtruyen.core.designsystem.theme.isDarkTheme
 import org.koin.compose.koinInject
 
 @Composable
@@ -50,9 +43,6 @@ fun Avatar(
             .background(backgroundColor)
             .padding(Dimens.Tiny)
     ) {
-        val themeMode by ThemePreferencesDatastore.themeMode.collectAsState(ThemePreferencesDatastore.Mode.SYSTEM)
-        Log.d("@@@", "ThemeMode: $themeMode")
-
         AsyncImage(
             model = imageLoader.load(context, imageUrl),
             fallback = painterResource(id = R.drawable.ic_fallback),
