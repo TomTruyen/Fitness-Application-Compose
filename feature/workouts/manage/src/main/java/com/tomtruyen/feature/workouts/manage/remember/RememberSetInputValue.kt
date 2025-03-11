f1cf3de33e520ac89ca59c815184fd9fa2c93421
Tom Truyen <tom.truyen@gmail.com>
2025-03-11 20:07:09 +0100

    fix: value issue

--- feature/workouts/manage/src/main/java/com/tomtruyen/feature/workouts/manage/remember/RememberSetInputValue.kt
+++ feature/workouts/manage/src/main/java/com/tomtruyen/feature/workouts/manage/remember/RememberSetInputValue.kt
@@ -1,9 +1,13 @@
 package com.tomtruyen.feature.workouts.manage.remember
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.tomtruyen.core.common.extensions.tryIntString
 import com.tomtruyen.core.common.models.ManageWorkoutMode
 
@@ -15,18 +19,24 @@ fun <T> rememberSetInputValue(
     transform: (T?) -> String = {
         when (it) {
             is Double? -> it?.tryIntString()
-            else -> it?.toString()
+            else -> it.toString()
         }.orEmpty()
     }
 ): Pair<MutableState<String>, T?> {
     val initial = remember { currentValue }
 
+    // Used to fix issue where initial == currentValue due to user input causing it to result in empty string
+    var initialRender by remember { mutableStateOf(true) }
+
     val value = remember(mode, currentValue, hasBeenCompleted) {
         val evaluation = (!mode.isExecute || initial != currentValue || hasBeenCompleted)
 
-        val transformedValue = if (evaluation) {
+        val transformedValue = if (evaluation || !initialRender) {
             transform(currentValue)
         } else ""
+
+        initialRender = false
+
         mutableStateOf(transformedValue)
     }
 
