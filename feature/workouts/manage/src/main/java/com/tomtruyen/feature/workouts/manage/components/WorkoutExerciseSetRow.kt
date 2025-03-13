35aebfae4af0812427396eda2e07f3d9f0b81173
Tom Truyen <tom.truyen@gmail.com>
2025-03-13 19:25:49 +0100

    fix: cleanup unused code

--- feature/workouts/manage/src/main/java/com/tomtruyen/feature/workouts/manage/components/WorkoutExerciseSetRow.kt
+++ feature/workouts/manage/src/main/java/com/tomtruyen/feature/workouts/manage/components/WorkoutExerciseSetRow.kt
@@ -1,6 +1,5 @@
 package com.tomtruyen.feature.workouts.manage.components
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
--- feature/workouts/manage/src/main/java/com/tomtruyen/feature/workouts/manage/remember/RememberSetInputValue.kt
+++ feature/workouts/manage/src/main/java/com/tomtruyen/feature/workouts/manage/remember/RememberSetInputValue.kt
@@ -1,6 +1,5 @@
 package com.tomtruyen.feature.workouts.manage.remember
 
-import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
