5542805662b67be11c7aaaa82809e62fd911aecd
Tom Truyen <tom.truyen@gmail.com>
2025-03-14 19:56:48 +0100

    fix: rounding of double

--- core/common/src/main/java/com/tomtruyen/core/common/extensions/DoubleExtensions.kt
+++ core/common/src/main/java/com/tomtruyen/core/common/extensions/DoubleExtensions.kt
@@ -1,9 +1,11 @@
 package com.tomtruyen.core.common.extensions
 
-import java.util.Locale
-
 fun Double.tryIntString(): String {
     val isInt = this % 1 == 0.0
 
-    return if (isInt) this.toInt().toString() else this.toString()
+    return if (isInt) this.toInt().toString() else this.rounded().toString()
+}
+
+fun Double.rounded(decimals: Int = 2): String {
+    return "%.${decimals}f".format(this)
 }
